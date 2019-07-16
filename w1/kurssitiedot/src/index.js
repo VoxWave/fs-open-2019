fb0e107fc0a8f0b962bb0d09d4bb9150ad0a0bb4
VoxWave <victor_bankowski@hotmail.com>
2019-07-16 12:59:28 +0300

    tehtävät 1.1 ja 1.2 tehty

--- w1/kurssitiedot/src/index.js
+++ w1/kurssitiedot/src/index.js
@@ -5,12 +5,20 @@ const Header = (props) => {
   return <h1>{props.course}</h1>
 }
 
-const Content = () => {
-
+const Content = (props) => {
+  var rendered = []
+  props.parts.forEach( e => {
+    rendered.push(<Part name = {e[0]} amount = {e[1]} />)
+  })
+  return rendered
 }
 
-const Total = () => {
+const Part = (props) => {
+  return <p>{props.name} {props.amount}</p>
+}
 
+const Total = (props) => {
+  return <p>Number of exercises {props.total}</p>
 }
 
 const App = () => {
@@ -24,17 +32,9 @@ const App = () => {
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header course = {course} />
+      <Content parts = {[[part1, exercises1], [part2, exercises2], [part3, exercises3]]} />
+      <Total total = {exercises1 + exercises2 + exercises3} />
     </div>
   )
 }
