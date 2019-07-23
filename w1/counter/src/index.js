14b0837e7d35bdcbbe96ece0211d29b23bdafe73
VoxWave <victor_bankowski@hotmail.com>
2019-07-23 16:31:15 +0300

    counter osuus tehty. workspace tiedosto luotu mukavuutta varten

--- w1/counter/src/index.js
+++ w1/counter/src/index.js
@@ -2,26 +2,27 @@ import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+const Display = ({counter}) => <div>{counter}</div>
+
+const Button = ({handleClick, text}) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
+
 const App = (props) => {
   const [counter, setCounter] = useState(0)
 
-  const increaseByOne = () => {
-    setCounter(counter + 1)
-  }
-
-  const setToZero = () => {
-    setCounter(0)
+  const setToValue = (value) => () => {
+      setCounter(value)
   }
 
   return (
     <div>
-      <div>{counter}</div>
-      <button onClick={increaseByOne}>
-        plus
-      </button>
-      <button onClick={setToZero}>
-        reset
-      </button>
+      <Display counter={counter}/>
+      <Button handleClick={setToValue(counter+1)} text='+'/>
+      <Button handleClick={setToValue(counter-1)} text='-'/>
+      <Button handleClick={setToValue(0)} text='reset'/>
     </div>
   )
 }
